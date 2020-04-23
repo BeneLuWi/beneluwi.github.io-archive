371ed8214855c53470cbf832c86f01e5d2fcacf4
Benedikt Lüken-Winkels <s4beluek@uni-trier.de>
2020-04-23 09:01:34 +0200

    rwg

---
+++ rwg/precache-manifest.21b126b2b2a6b8a4d8e0492d57afcef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6514c769005ec03f6ce02ea12ec72f54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c405cacbfa5e628cfc34",
+    "url": "/static/css/main.965c906f.chunk.css"
+  },
+  {
+    "revision": "c1271e6030a11b52568c",
+    "url": "/static/js/2.a25353ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a25353ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c405cacbfa5e628cfc34",
+    "url": "/static/js/main.3335f6a2.chunk.js"
+  },
+  {
+    "revision": "8bb5a64846a064d4c6ad",
+    "url": "/static/js/runtime-main.6475b9c5.js"
+  }
+]);
